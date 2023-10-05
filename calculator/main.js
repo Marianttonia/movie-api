3dae99cdb77c3d96c20316645f0c0e562efb3cc0
Maria Antonia <marianttonia054@gmail.com>
2023-10-05 16:34:09 -0300

    calculator_init

---
+++ calculator/main.js
@@ -0,0 +1,45 @@
+// document.body.innerHTML = "<h1>Calculator</h1>";
+
+// var operators = document.querySelector(".operators");
+// var num1 = document.getElementById("1");
+// var num2 = document.querySelector(".numbers");
+
+// var name = document.createElement("bold").textContent("oi")
+// // console.log(`name`, name)
+// num1.appendChild(name);
+
+function test (tag, expected) {
+    var expected = tag.document.write(tag)
+    console.log("temp", tag);
+    console.log(expected)
+};    
+
+function example(number1, number2) {
+    console.log(number1 / number2)
+}
+
+
+function calculator () {
+    if (operators == "+") { 
+        operacao = num1 + num2;
+    };
+    if (operators == "-") {
+        operacao = num1 - num2;
+    };
+    if (operators == "%") {
+        operacao = num1 / num2;
+    };
+    if (operators == "x") {
+        operacao = num1 * num2; 
+    };
+    return operacao;
+};
+
+function validation() {
+    var operacao = ""
+    if (operacao == "") {
+        document.write("Digite a operacao desejada");
+    } else {
+        document.write("O valor da operacao e: " , calculator(operacao));
+    };
+};
