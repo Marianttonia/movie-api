191e8ff841b10e4be486a84ae0d955e077f55a23
Marianttonia <marianttonia054@gmail.com>
2023-10-30 11:55:59 -0300

    operators_precedence

--- calculator/main.js
+++ calculator/main.js
@@ -1,56 +1,82 @@
+document.addEventListener("onclick", handleCharacter);
+
 var grid = [];
+var precedence = ["(","[","{"];
+var operators = ["x","/","+","-"];
 var operacao = "";
-var firstNumber, operator, finalNumber, resultRegex;
+var firstNumber, operator, finalNumber, resultRegex, openParenteses, closeParenteses, contentParenteses, lengthPatenteses, newRegex;
 
 function handleCharacter(character){
-  if (character.id === "cleanDigit"){
-    grid = grid.slice(0,-1);
-  } else if (character.id === "cleanAll") {
-    grid = "";
-  } else {
-    grid += character.id;
+if (character.id === "cleanDigit"){
+  grid = grid.slice(0,-1);
+} else if (character.id === "cleanAll") {
+  grid = "";
+} else if (validation()) {
+  grid += character.id;
+};
+gridhtml.innerHTML = grid;
+
+function verificationData(responseVerification, errorOperator) {
+  if (grid == "") {
+    alert("Digite a operacao desejada");
+  } else if (
+    // precende +1: error
+  )
+}
+
+const regex = /(\d+)|(\w)|(%)|(\+)|(-)|[/(\d+\.\d+)/)|(\w)|(%)|(\+)|(-)(\d+)/)/]/g;
+// (\d+\.\d+)|(\w)|(%)|(\+)|(-)|\([(\d+\.\d+)|(\w)|(%)|(\+)|(-)(\d+\.\d+)\)]
+resultRegex = grid.match(regex);
+console.log("result regex", resultRegex);
+
+
+function find_index (arr, value) {
+  const indexes = [];
+    for (let = i in arr) {
+        if (arr[i] === value);   {
+          indexes.push(i);
+      }; 
   };
-  gridhtml.innerHTML = grid;
- 
-  const regex = /(\d+)|(\w)|(%)|(-)|(\+)|(\d+)/g;
-  resultRegex = grid?.match(regex);
-  firstNumber = Number(resultRegex[0]);
-  operator = resultRegex[1];
-  finalNumber = Number(resultRegex[2]);
 };
 
-function calculator (firstNumber, operator, finalNumber) {
-    if (operator === "+") { 
-        operacao = firstNumber + finalNumber;
+function operations {
+  operators.map(el => { 
+    position = operators.map((el) => el === find_index(grid));
+    if (position !== -1) {
+        var beforePosition = position-1;
+        var afterPosition = position+1;
+        console.log("positions by map" ,beforePosition, position, afterPosition);
+        calculator(beforePosition, position, afterPosition);
     };
-    if (operator === "-") {
-        operacao = firstNumber - finalNumber;
-    };
-    if (operator === "%") {
-        operacao = firstNumber / finalNumber;
-    };
-    if (operator === "x") {
-        operacao = firstNumber * finalNumber; 
-    };
-    return operacao;
+  });
 };
 
-function validation() {
-    if (grid === "") {
-        alert("Digite a operacao desejada");
-    } else {
-      i = 0;
-      while (i < resultRegex.length) {
-        var result = calculator(firstNumber, operator, finalNumber);
-        firstNumber = Number(result);
-        operator = resultRegex[1];
-        finalNumber = Number(resultRegex[2]);
-        console.log(resultRegex,firstNumber,operator,finalNumber);
-        i++;
-        };
-      
-        result = calculator(firstNumber, operator, finalNumber);
-        gridhtml.innerHTML = result;
-        grid = result;
-    };
+function calculator(beforePosition, position, afterPosition){
+    firstNumber = Number(grid.find((e, i) => i === beforePosition));
+    operator = grid.find((element, index) => index === position);
+    finalNumber = Number(grid.find((element, index) => index === afterPosition));
+    console.log("pars calc" ,firstNumber, operator, finalNumber);
+
+  if (operator === "%") {
+      result = firstNumber / finalNumber;
+  } else if (operator === "x") {
+      result = firstNumber * finalNumber; 
+  } else if (operator === "+") { 
+      result = firstNumber + finalNumber;
+  } else if (operator === "-") {
+      result = firstNumber - finalNumber;
+  };
+
+  grid.splice(beforePosition, 3, result);
+    console.log("grid" ,grid);
+    console.log("operacao" ,result);
+  return result;
+};
+
+function responseCalculator() {
+  if (verificationData().response === "false") {
+      alert(responseVerification());
+  };
+  grid = result;
+  gridhtml.innerHTML = result;
 };
