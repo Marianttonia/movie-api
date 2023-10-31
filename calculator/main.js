0d9e44698568afbabd953df2a5a5533a71ab6c75
Marianttonia <marianttonia054@gmail.com>
2023-10-31 10:20:42 -0300

    errors_modify

--- calculator/main.js
+++ calculator/main.js
@@ -1,82 +1,79 @@
-document.addEventListener("onclick", handleCharacter);
+var grid = []
+var precedence = ["(","[","{"]
+var operators = ["x","/"]
+var operacao = ""
+var firstNumber, operator, finalNumber, resultRegex, openParenteses, closeParenteses, contentParenteses, lengthPatenteses, newRegex
 
-var grid = [];
-var precedence = ["(","[","{"];
-var operators = ["x","/","+","-"];
-var operacao = "";
-var firstNumber, operator, finalNumber, resultRegex, openParenteses, closeParenteses, contentParenteses, lengthPatenteses, newRegex;
-
-function handleCharacter(character){
-if (character.id === "cleanDigit"){
-  grid = grid.slice(0,-1);
-} else if (character.id === "cleanAll") {
-  grid = "";
-} else if (validation()) {
-  grid += character.id;
-};
-gridhtml.innerHTML = grid;
-
-function verificationData(responseVerification, errorOperator) {
-  if (grid == "") {
-    alert("Digite a operacao desejada");
-  } else if (
-    // precende +1: error
-  )
+function handleCharacter(character) {
+  if (character.id === 'cleanDigit'){
+    grid = grid.slice(0, -1)
+  } else if (character.id === 'cleanAll') {
+    grid = ''
+  } else {
+    grid += character.id
+  }
+  gridhtml.innerHTML = grid
+  const regex = /(\d+)|(\w)|(%)|(\+)|(-)|[/(\d+\.\d+)/)|(\w)|(%)|(\+)|(-)(\d+)/)/]/g
+  resultRegex = grid.match(regex)
 }
 
-const regex = /(\d+)|(\w)|(%)|(\+)|(-)|[/(\d+\.\d+)/)|(\w)|(%)|(\+)|(-)(\d+)/)/]/g;
-// (\d+\.\d+)|(\w)|(%)|(\+)|(-)|\([(\d+\.\d+)|(\w)|(%)|(\+)|(-)(\d+\.\d+)\)]
-resultRegex = grid.match(regex);
-console.log("result regex", resultRegex);
+function verificationData() {
+  if (grid == '') {
+    alert("Digite a operacao desejada");
+  }
+}
 
+function findIndex(arr) {
+  const indexes = []
+  for (let i in arr) {
+    console.log(arr[i])
+    if (arr[i] === 'x' || arr[i] === '/') {
+      indexes.push(i)
+    }
+  }
+  return indexes
+}
 
-function find_index (arr, value) {
-  const indexes = [];
-    for (let = i in arr) {
-        if (arr[i] === value);   {
-          indexes.push(i);
-      }; 
-  };
-};
-
-function operations {
-  operators.map(el => { 
-    position = operators.map((el) => el === find_index(grid));
-    if (position !== -1) {
-        var beforePosition = position-1;
-        var afterPosition = position+1;
-        console.log("positions by map" ,beforePosition, position, afterPosition);
-        calculator(beforePosition, position, afterPosition);
-    };
-  });
-};
+operators.map(el, positions => { 
+  var positions = findIndex(resultRegex)
+  if (positions.length !== 0) {
+    positions.forEach(position => {
+      var beforePosition = position - 1
+      var afterPosition = position + 1
+      console.log("positions by map" ,beforePosition, position, afterPosition)
+      calculator(beforePosition, position, afterPosition)
+    })
+  } else {
+    position = resultRegex[1];
+    beforePosition = resultRegex[0];
+    afterPosition = resultRegex[2];
+    console.log(resultRegex,firstNumber,operator,finalNumber);      
+    result = calculator(firstNumber, operator, finalNumber)
+}})
 
 function calculator(beforePosition, position, afterPosition){
-    firstNumber = Number(grid.find((e, i) => i === beforePosition));
-    operator = grid.find((element, index) => index === position);
-    finalNumber = Number(grid.find((element, index) => index === afterPosition));
-    console.log("pars calc" ,firstNumber, operator, finalNumber);
-
-  if (operator === "%") {
-      result = firstNumber / finalNumber;
-  } else if (operator === "x") {
-      result = firstNumber * finalNumber; 
-  } else if (operator === "+") { 
-      result = firstNumber + finalNumber;
-  } else if (operator === "-") {
-      result = firstNumber - finalNumber;
-  };
-
-  grid.splice(beforePosition, 3, result);
-    console.log("grid" ,grid);
-    console.log("operacao" ,result);
-  return result;
-};
+  firstNumber = Number(grid.find((e, i) => i === beforePosition))
+  operator = grid.find((element, index) => index === position)
+  finalNumber = Number(grid.find((element, index) => index === afterPosition))
+  console.log("pars calc" ,firstNumber, operator, finalNumber)
+  if (operator === "/") {
+    result = firstNumber / finalNumber
+  }
+  if (operator === "x") {
+    result = firstNumber * finalNumber;
+  }
+  if (operator === "+") { 
+    result = firstNumber + finalNumber
+  }
+  if (operator === "-") {
+    result = firstNumber - finalNumber
+  }
+  grid.splice(beforePosition, 3, result)
+  return result
+}
 
 function responseCalculator() {
-  if (verificationData().response === "false") {
-      alert(responseVerification());
-  };
-  grid = result;
-  gridhtml.innerHTML = result;
-};
+  grid = result
+  console.log(result)
+  gridhtml.innerHTML = result
+}
