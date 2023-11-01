669ea9191ca14d4b8c7b6ef6b658e994f44f5e06
Marianttonia <marianttonia054@gmail.com>
2023-11-01 00:58:15 -0300

    add_operations

--- calculator/main.js
+++ calculator/main.js
@@ -1,6 +1,5 @@
 var grid = []
 var precedence = ["(","[","{"]
-var operators = ["x","/"]
 var operacao = ""
 var firstNumber, operator, finalNumber, resultRegex, openParenteses, closeParenteses, contentParenteses, lengthPatenteses, newRegex
 
@@ -11,10 +10,15 @@ function handleCharacter(character) {
     grid = ''
   } else {
     grid += character.id
-  }
-  gridhtml.innerHTML = grid
-  const regex = /(\d+)|(\w)|(%)|(\+)|(-)|[/(\d+\.\d+)/)|(\w)|(%)|(\+)|(-)(\d+)/)/]/g
-  resultRegex = grid.match(regex)
+    gridhtml.innerHTML = grid
+    const regex = /(\d+)|(\w)|(%)|(\+)|(-)|[/(\d+\.\d+)/)|(\w)|(%)|(\+)|(-)(\d+)/)/]/g
+    resultRegex = grid.match(regex)
+    if (character.id === 'result') {
+      coreCalculator()
+      grid = result
+      gridhtml.innerHTML = grid
+    }
+  } // need call the validations after precedence
 }
 
 function verificationData() {
@@ -23,57 +27,46 @@ function verificationData() {
   }
 }
 
-function findIndex(arr) {
-  const indexes = []
-  for (let i in arr) {
-    console.log(arr[i])
-    if (arr[i] === 'x' || arr[i] === '/') {
-      indexes.push(i)
-    }
-  }
-  return indexes
-}
+function coreCalculator() {
+  var operatorIndex;
 
-operators.map(el, positions => { 
-  var positions = findIndex(resultRegex)
-  if (positions.length !== 0) {
-    positions.forEach(position => {
-      var beforePosition = position - 1
-      var afterPosition = position + 1
-      console.log("positions by map" ,beforePosition, position, afterPosition)
-      calculator(beforePosition, position, afterPosition)
-    })
-  } else {
-    position = resultRegex[1];
-    beforePosition = resultRegex[0];
-    afterPosition = resultRegex[2];
-    console.log(resultRegex,firstNumber,operator,finalNumber);      
-    result = calculator(firstNumber, operator, finalNumber)
-}})
-
-function calculator(beforePosition, position, afterPosition){
-  firstNumber = Number(grid.find((e, i) => i === beforePosition))
-  operator = grid.find((element, index) => index === position)
-  finalNumber = Number(grid.find((element, index) => index === afterPosition))
-  console.log("pars calc" ,firstNumber, operator, finalNumber)
-  if (operator === "/") {
-    result = firstNumber / finalNumber
+  while ((operatorIndex = resultRegex.findIndex(el => el === 'x' || el === '/')) !== -1) {
+    var previus = Number(operatorIndex) 
+    var beforePrevius = Number(previus-1)
+    var afterPrevius = Number(previus+1)
+    console.log("positions by map precedence" ,beforePrevius, previus, afterPrevius)
+    calculator(beforePrevius, previus, afterPrevius)
   }
-  if (operator === "x") {
-    result = firstNumber * finalNumber;
-  }
-  if (operator === "+") { 
-    result = firstNumber + finalNumber
-  }
-  if (operator === "-") {
-    result = firstNumber - finalNumber
-  }
-  grid.splice(beforePosition, 3, result)
-  return result
-}
-
-function responseCalculator() {
-  grid = result
   console.log(result)
-  gridhtml.innerHTML = result
+
+  while ((operatorIndex = resultRegex.findIndex(el => el === '+' || el === '-')) !== -1) {
+    var previus = Number(operatorIndex) 
+    var beforePrevius = Number(previus-1)
+    var afterPrevius = Number(previus+1)
+    console.log("positions by map precedence" ,beforePrevius, previus, afterPrevius)
+    calculator(beforePrevius, previus, afterPrevius)
+  }
 }
+
+function calculator(beforePrevius, previus, afterPrevius){
+    firstNumber = Number(resultRegex[beforePrevius])
+    operator = resultRegex[previus] 
+    finalNumber = Number(resultRegex[afterPrevius]) //
+    console.log("pars calc" ,firstNumber, operator, finalNumber)
+    if (operator === "/") {
+        result = firstNumber / finalNumber
+    }
+    if (operator === "x") {
+        result = firstNumber * finalNumber;
+    }
+    if (operator === "+") { 
+        result = firstNumber + finalNumber
+    }
+    if (operator === "-") {
+        result = firstNumber - finalNumber
+    }
+    resultRegex.splice(beforePrevius, 3, result) // 
+    console.log(result, resultRegex)
+}
+
+console.log(coreCalculator())
