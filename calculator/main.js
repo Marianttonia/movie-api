fb76d87f84eb239293ddf0205f9211ec029e757a
Marianttonia <marianttonia054@gmail.com>
2023-11-06 01:00:27 -0300

    finish_error_handling

--- calculator/main.js
+++ calculator/main.js
@@ -2,9 +2,8 @@ var grid = ''
 var resultValue = ''
 var firstNumber, operator, finalNumber, resultRegex, manipulationCalcule, operatorIndex, limitIndex, firstfinaly
 const regex = /(\d+(\.\d+)?|[\+\-\*\/x(){}\[\]])/g
+const consecutivos = ['x', '/', '+', '-']
 
-// resultRegex = ['3', '-', '(', '+', '7', ')', 'x', '(', '-', '5', ')'] 
-// resultRegex = ['8', '-', '(', '6', ')']
 
 function handleCharacter(character) {
   grid += character.id
@@ -20,18 +19,19 @@ function cleanAll() {
   gridhtml.innerHTML = grid
 }
 
-// validation()
 
 function validation() {
+  console.log(resultRegex)
   if (grid == '') {
     alert("Digite a operacao desejada");
   } else {
     try {
       result()
-      gridhtml.innerHTML = resultValue
-      console.log(resultValue, resultRegex, manipulationCalcule)
+      gridhtml.innerHTML = resultValue.toFixed(2)
+      alert(`O resultado é ${resultValue.toFixed(2)} :) `)
     } catch(e) {
-      alert('Digite a expressao corretamente' , e.messsage)
+      console.log('deu erro rs, digite a operação corretamente')
+      alert('Digite a expressao corretamente, para cada operador, dois operandos. E para cada seletor de prioridade, seu fechamento. ', e.message)
       alert('Para positivos use (+n) e negativos (-n)')
     }
   }
@@ -43,53 +43,83 @@ function result() {
   if ((precedenceIndex = resultRegex.findIndex(el => el === '(')) !== -1) {
     if (limitIndex = resultRegex.findIndex(limit => limit === ')' ))
         integerCheck()
-        precedenceOrder(precedenceIndex) // até q ok
+        precedenceOrder(precedenceIndex)
       }
-      if ((precedenceIndex = resultRegex.findIndex(el => el === '[')) !== -1) {
+    if ((precedenceIndex = resultRegex.findIndex(el => el === '[')) !== -1) {
         if (limitIndex = resultRegex.findIndex(limit => limit === ']' )) {
           integerCheck()
-          precedenceOrder(precedenceIndex)
+          precedenceOrder(precedenceIndex) // deve devolver aqui a regex splicetada do conteudo []
         } 
       } 
-      if ((precedenceIndex = resultRegex.findIndex(el => el === '{')) !== -1) {
+    if ((precedenceIndex = resultRegex.findIndex(el => el === '{')) !== -1) {
         if (limitIndex = resultRegex.findIndex(limit => limit === '}' )) {
           integerCheck()
           precedenceOrder(precedenceIndex)
         } 
-      } // verify finaly etapa integercheck
-      precedenceOrder(precedenceIndex)
-      if (isNaN(resultValue)) {
-        throw Error ('Expressao invalida')
-      } // recebe o result value e o tratamento de erro ja foi feito
-} console.log(resultValue)
+      } 
+    if (resultRegex.length > 1) {
+        coreCalculator()
+      }
+  if (isNaN(resultValue)) {
+      throw Error ('Expressao invalida')
+  } else {
+      resultRegex = manipulationCalcule
+      return resultValue
+  }
+}
+
 
 function precedenceOrder(precedenceIndex) {
   if (manipulationCalcule.length > 1) {
     var initIndex = Number(precedenceIndex+1)
     manipulationCalcule = manipulationCalcule.slice(initIndex, limitIndex)
     calculeLength = (Number(manipulationCalcule.length)) +2
-    coreCalculator() // call corecalculator, AQUI ESTÁ O ERRO DA ÚLTIMA CHAMADA
-    if (manipulationCalcule.length > 1) {
+    coreCalculator() 
+    if(manipulationCalcule.length > 1 || resultRegex.length > 3) { // se não tiver mais o que calcular
       resultRegex.splice(precedenceIndex, calculeLength)
       resultRegex.splice(precedenceIndex, 0, resultValue)
       manipulationCalcule = resultRegex
     }
   }
-    resultRegex = manipulationCalcule
-    return resultValue
-}
-
-    
-function coreCalculator() {
-  if ((operatorIndex = manipulationCalcule.findIndex(el => el === 'x' || el === '/')) !== -1){
-    calculator(operatorIndex-1, operatorIndex, operatorIndex+1)
-  }
-  if ((operatorIndex = manipulationCalcule.findIndex(el => el === '+' || el === '-')) !== -1) {
-    calculator(operatorIndex-1, operatorIndex, operatorIndex+1)
-  }
   return resultValue
 }
-      
+    
+
+function coreCalculator() {
+  if (validationOperators(manipulationCalcule, consecutivos) === true && consecutivos.includes(manipulationCalcule[0]) === false) {
+      while ((operatorIndex = manipulationCalcule.findIndex(el => el === 'x' || el === '/')) !== -1){
+        calculator(operatorIndex-1, operatorIndex, operatorIndex+1)
+      }
+      while ((operatorIndex = manipulationCalcule.findIndex(el => el === '+' || el === '-')) !== -1) {
+        calculator(operatorIndex-1, operatorIndex, operatorIndex+1)
+      }
+      return resultValue
+    } else { 
+    manipulationCalcule = []
+    return resultValue = NaN
+  }
+}
+
+
+function validationOperators(manipulationCalcule, consecutivos) {
+  let contador = 0
+  for (let i = 0; i < manipulationCalcule.length; i++) {
+      if (consecutivos.includes(manipulationCalcule[i])) {
+      contador++
+          if (contador >= 2) {
+            alert('Você deve digitar um operador por vez')
+          return false
+      }
+      } else {
+          contador = 0
+      }
+  }
+  if (manipulationCalcule.length > 2 ) {
+    return true;
+  }
+}
+
+
 function calculator(beforePrevius, previus, afterPrevius){
   firstNumber = Number(manipulationCalcule[beforePrevius])
   operator = manipulationCalcule[previus] 
@@ -107,25 +137,26 @@ function calculator(beforePrevius, previus, afterPrevius){
   if (operator === '-') {
       resultValue = firstNumber - finalNumber
   }
-  if (manipulationCalcule.length > 0) {
+  // possivel erro ao mandar para o precende index, mudando o valor da lista
+  if (manipulationCalcule.length > 0) { 
       manipulationCalcule.splice(beforePrevius, 3, resultValue) 
       console.log(resultValue, manipulationCalcule)
   }
   return resultValue
 }
 
+
 function integerCheck() {
   if (firstClose = resultRegex.findIndex(firstFinaly => firstFinaly === ')')) {
     var verifyRational = manipulationCalcule.slice(firstClose-2, firstClose)
     var firstOpen = Number(firstClose-3)
     if (resultRegex[firstOpen] === '(' ) {
-      verifyRational = Number(verifyRational.join('')) // take fist numb okay
+      verifyRational = Number(verifyRational.join('')) 
         if (Math.sign(verifyRational) !== NaN) { 
-          resultRegex.splice(firstOpen, 4)
+          resultRegex.splice(firstOpen, 4) // atualiza a resultRegex
           resultRegex.splice(firstOpen, 0, verifyRational)
           result()
       }
     } 
   }
 } 
-
