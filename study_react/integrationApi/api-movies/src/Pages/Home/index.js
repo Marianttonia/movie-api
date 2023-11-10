611c088cf0009d372a7425cf422f4b8c398a34d3
Maria Antonia <marianttonia054@gmail.com>
2023-11-10 17:26:15 -0300

    add_fetch

--- study_react/integrationApi/api-movies/src/Pages/Home/index.js
+++ study_react/integrationApi/api-movies/src/Pages/Home/index.js
@@ -1,41 +1,32 @@
 // wrhite the page
-import image from './smileviolet.jpg'
+import image from './smileviolet.jpg';
 import { Container, MovieList, Movie } from './style';
+import { useState, useEffect } from 'react';
+import { apikey } from '../../config/key';
 
 function Home() {
 
-    const movies = [
-        {
-            title: 'Spider man',
-            img: './smileviolet.jpg'
-        },
-        {
-            title: 'Violet Evergarden',
-            img: './smileviolet.jpg'
-        },
-        {
-            title: 'Batgirl',
-            img: './smileviolet.jpg'
-        }
-    ]
-    
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.themoviedb.org')
+    }, [])
+
     return (
         <Container>
             <h1> Movies </h1>
-
-            <MovieList>
-
-                {movies.map(movie =>  {
+            {/* Use map for return. ForEach dont return */}
+            <MovieList> 
+                {movies.map((movie) => {
                     return (
-                        <Movie>
-                        <a href='https://gogole.com.br'>
-                            <img src={image} alt={movie.title}/>
-                        </a> 
-                        <span> {movie.title} </span>
-                    </Movie>
-                    )}
-                )}
-
+                        <Movie key={movie.id}>
+                            <a href="https://gogole.com.br">
+                                <img src={image} alt={movie.title} />
+                            </a>
+                            <span> {movie.title} </span>
+                        </Movie>
+                    );
+                })}
             </MovieList>
         </Container>
     );
---
+++ study_react/integrationApi/api-movies/src/config/key.js
@@ -0,0 +1,1 @@
+export const apikey =  '0a7228e731bf6d46058f564809890d6'
