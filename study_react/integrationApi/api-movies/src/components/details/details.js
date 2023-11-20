cc0c649337be37deebbc82f759d19c29ea498cb6
Maria Antonia <marianttonia054@gmail.com>
2023-11-20 15:23:32 -0300

    finish_carousel

--- study_react/integrationApi/api-movies/src/components/details/details.js
+++ study_react/integrationApi/api-movies/src/components/details/details.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import styles from './details.module.css';
 
-const Details = ({movie, img}) => {
-    const {id, overview, title} = movie;
+const Details = ({movieDetails, img_path}) => {
+    const {backdrop_path, title, overview, release_date, production_countries, genres, runtime, vote_average } = movieDetails;
 
+    const country = production_countries.length > 0 ? production_countries[0].name : '';
+    const genreNames = genres.map((genre) => genre.name).join(', ');
+
+    return (
+        <div className={styles.container} >
+            <div className={styles.movie} >
+                <img className={styles.img} src={`${img_path}${backdrop_path}`} alt={title} />  
+            </div>
+            <div className={styles.movieDetails} >
+                <h1> {title} </h1>
+                <section className={styles.info}>
+                    <p> {release_date} </p>
+                    <p> {country} </p>
+                    <p> {runtime} min </p>
+                </section>
+                <section className={styles.genres} >
+                    <p> {genreNames} </p>
+                    <p> Average: {vote_average} </p>
+                </section>
+                <p className={styles.overview} > {overview} </p>
+            </div>
+        </div>
+    )
 };
 
 export default Details;
--- study_react/integrationApi/api-movies/src/pages/movie/moviePage.js
+++ study_react/integrationApi/api-movies/src/pages/movie/moviePage.js
@@ -1,31 +1,33 @@
 
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import { useParams } from 'react-router-dom';
 import { getMovieDetails } from '../../services/api';
-import Details from '../../components/movieDetails/details';
-
+import Details from '../../components/details/details';
 const MoviePage = () => {
   const { id } = useParams();
   const [movieDetails, setMovieDetails] = useState(null);
   const img_path = 'https://image.tmdb.org/t/p/w500';
 
-  const findMovieDetails = async () => {
+  const findMovieDetails = useCallback(async () => {
     try {
       const details = await getMovieDetails(id);
       setMovieDetails(details);
     } catch (e) {
-      console.log('Erro ao carregar pagina de detalhes do filme: ', e)
+      console.log('Erro ao carregar página de detalhes do filme: ', e);
     }
-  }
+  }, [id]);
+
   useEffect(() => {
     findMovieDetails();
-  }, [id]);
+  }, [findMovieDetails]);
     if (!movieDetails) {
     return <div> Loading...</div>;
   }
 
   return (
-    <Details movieDetails={movieDetails} img_path={img_path} ></Details>
+    <div>
+      <Details  movieDetails={movieDetails} img_path={img_path} ></Details>
+    </div>
   );
 };
 
